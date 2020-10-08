77ab8eadd82a93c2517b7f97f009a0dbb61a7de2
Aum1234 <lalida.am.62@ubu.ac.th>
2020-10-08 10:37:44 +0000

    8/10/20

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from mywed import views
+from django.contrib.auth import views as aunt_views
 
 urlpatterns = [
-	path('', views.index),
-	path('mywed/', include('mywed.urls')),
-	path('polls/', include('polls.urls')),
+    path('', aunt_views.LoginView.as_view(template_name='mywed/login.html'), name='login'),
+	path('index', views.index , name='index'),
+	path('signup', views.signup, name='signup'),
+    path('logout',views.logout, name='logout'),
+#	path('mywed/', include('mywed.urls')),
 	path('admin/', admin.site.urls),
 ]
--- mywed/urls.py
+++ mywed/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
+from django.contrib.auth import views as aunt_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', aunt_views.LoginView.as_view(template_name='mywed/login.html'), name='login'),
+    path('index', views.index, name='index'),
+    path('signup', views.signup, name='signup'),
+    path('logout',views.logout, name='logout'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
